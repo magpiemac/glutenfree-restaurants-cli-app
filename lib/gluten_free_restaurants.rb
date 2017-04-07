dd0194724165040a6409a2536f079adf14fd37dd
Shannon McCormack <shannonkmc@hotmail.com>
2017-04-07 19:41:34 +0000

    Stubbed out cli code.

--- lib/gluten_free_restaurants.rb
+++ lib/gluten_free_restaurants.rb
@@ -1,7 +1,2 @@
 require_relative "./gluten_free_restaurants/version.rb"
-
-module GlutenFreeRestaurants
-  # Your code goes here...
-end
-
 require_relative "./gluten_free_restaurants/cli.rb"
--- lib/gluten_free_restaurants/cli.rb
+++ lib/gluten_free_restaurants/cli.rb
@@ -3,7 +3,32 @@
 class GlutenFreeRestaurants::CLI
 
   def call
-    puts "Welcome to Gluten Free Restaurants in Annapolis, MD"
+    puts "Welcome to Gluten Free Restaurants in Annapolis, MD:"
+    list_restaurants
+    restaurant_info
+    goodbye
   end
 
+  def list_restaurants
+    puts "1. Giant Foods"
+    puts "2. Jerry's Subs and Pizza"
+  end
+
+  def restaurant_info
+    puts "Enter the number of the restaurant that you would like more information on or type exit:"
+    input = nil
+    while input != "exit"
+      input = gets.strip
+      case input
+      when "1"
+        puts "More info on restaurant 1..."
+      when "2"
+        puts "More info on restaurant 2..."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thank you. Come back soon."
+  end
 end
