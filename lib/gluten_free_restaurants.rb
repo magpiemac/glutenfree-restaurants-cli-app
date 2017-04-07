5a861bb872529e494516ab2429e0abbd9f16b71f
Shannon McCormack <shannonkmc@hotmail.com>
2017-04-07 19:23:59 +0000

    Added require_relative files and updated gemspec text.

--- lib/gluten_free_restaurants.rb
+++ lib/gluten_free_restaurants.rb
@@ -1,5 +1,7 @@
-require "gluten_free_restaurants/version"
+require_relative "./gluten_free_restaurants/version.rb"
 
 module GlutenFreeRestaurants
   # Your code goes here...
 end
+
+require_relative "./gluten_free_restaurants/cli.rb"
