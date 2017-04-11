4f9bb5eb154e3eacecd8bf08aecd6d6c65f172fe
Shannon McCormack <shannonkmc@hotmail.com>
2017-04-11 16:16:49 +0000

    Added code for self.scrape method in gfrestaurants.rb.

--- lib/gluten_free_restaurants/gfrestaurants.rb
+++ lib/gluten_free_restaurants/gfrestaurants.rb
@@ -23,7 +23,10 @@ class GlutenFreeRestaurants::Restaurants
 
   def self.scrape
     doc = Nokogiri::HTML(open("https://www.findmeglutenfree.com/search?a=Annapolis%2C%20MD&lat=38.9784&lng=-76.4922&ga=Annapolis%2C%20MD"))
-    name = doc.css("div.search-list-title").text.strip
+    name = doc.search("a.data-details-url")[0].text
+    location = doc.search("h4.data-address")[0].text
+    features = doc.search("h3")[0].text
+    #url = doc.search("a.href").text
     #binding.pry
   end
 end
